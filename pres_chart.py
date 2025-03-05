4669e6aafb205aa02f305f8d687f2c536970f08b
1609290 <hurrenj@cardiff.ac.uk>
2025-03-05 15:35:50 +0000

    Added header title

--- pres_chart.py
+++ pres_chart.py
@@ -2,6 +2,10 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
+# Set page config
+st.set_page_config(page_title="Payments Intelligence interactive column chart", layout="wide")
+
+
 # Load the dataset
 @st.cache_data
 def load_data():
@@ -41,8 +45,13 @@ def generate_column_chart(x_axis: str):
     return fig
 
 
-# Set page config
-st.set_page_config(page_title="Payments Intelligence interactive column chart", layout="wide")
+
+
+# Centered title using markdown and HTML
+st.markdown(
+    "<h1 style='text-align: center;'>Payments Intelligence</h1>", 
+    unsafe_allow_html=True
+)
 
 st.subheader("What do high-value transactions look like?")
 category_col = st.selectbox("Select a category to see the value of transactions by payment method:", ["Customer Segment", "Merchant Category", "Sales Channel", "Location", "Customer Device Type", "Promotion/Discount Applied"])
